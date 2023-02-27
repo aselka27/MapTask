b36dee7514b899613e6ddc15a5ee09b3ee5f30aa
aselka27 <asjumalieva@gmail.com>
2023-02-27 23:14:15 +0600

    Initial Commit

---
+++ MapTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapTask
+//
+//  Created by саргашкаева on 27.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
